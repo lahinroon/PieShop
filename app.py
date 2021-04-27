abb991b2c5383435573b5949a831c97084aa984c
Nihal <nihalnoor007@gmail.com>
2021-04-27 21:18:42 +1000

    Pie shop initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect, url_for, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/pies")
+def pies():
+    return render_template("pies.html")    
+
+if __name__== "__main__":
+    app.run(debug=True) 
